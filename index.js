900196e7f73c771a1dead93a6e38b006e211b474
TwentiethGuardian2020 <66183412+TwentiethGuardian2020@users.noreply.github.com>
2020-06-03 12:54:29 -0700

    fixed moirainequote

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ Discord.on('message', async (msg) => {
     if (msg.content.startsWith('>remind')) {
     msg.channel.send(`WRITE, YOU FOOL, WRITE! ${msg.mentions.members.first()}`);
     }
-Discord.on('message', async (msg) => {
-    if (msg.author.bot) return;
     if (msg.content.startsWith('>moirainequote')) {
     msg.channel.send(`The Wheel weaves as the Wheel wills. ${msg.mentions.members.first()}`);
     }
-  })
 });
 Discord.login(process.env.TOKEN)
