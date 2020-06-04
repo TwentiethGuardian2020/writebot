c637e4c24a4a6b0d0ca67b5e75bee942026ee4cf
TwentiethGuardian2020 <66183412+TwentiethGuardian2020@users.noreply.github.com>
2020-06-03 19:29:36 -0700

    removed commands

--- index.js
+++ index.js
@@ -5,36 +5,16 @@ Discord.on('message', async (msg) => {
     if (msg.content.startsWith('>remind')) {
     msg.channel.send(`WRITE, YOU FOOL, WRITE! ${msg.mentions.members.first()}`);
     }
-    if (msg.content.startsWith('>moirainequote')) {
+    if (msg.content.startsWith('>wotquote1')) {
     msg.channel.send(`"The Wheel weaves as the Wheel wills."`);
     }
-    if (msg.content.startsWith('>randquote1')) {
-    msg.channel.send(` || "Here is your flaw, Shaitan, Lord of the Dark, Lord of Envy, Lord of Nothing, here is why you fail. It was not about me. It’s never been about me.”
-    It was about a woman, torn and beaten down, cast from her throne and made a puppet. A woman who had crawled when she had to. That woman still fought.
-    It was about a man that love repeatedly forsook. A man who found relevance in a world that others would have let pass them by. A man who remembered stories and who took fool boys under his wing when the smarter move would have been to keep on walking. That man still fought.
-    It was about a woman with a secret, a hope for the future. A woman who had hunted the truth before others could. A woman who had given her live, then had it returned. That woman still fought.
-    It was about a man whose family was taken from him, but who stood tall in his sorrow and protected those he could.
-    It was about a woman who refused to believe that she could not help, could not heal those who had been harmed.
-    It was about a hero who insisted with every breath that he was anything but a hero.
-    It was about a woman who would not bend her back while she was beaten, and who shown with a light for all who watched, including Rand.
-    It was about them all." || `);
-    }
-    if (msg.content.startsWith('>randquote2')) {
-    msg.channel.send(`"What can be done... can be undone."`);
-    }
-    if (msg.content.starsWith('>randquote3')) {
-    msg.channel.send(' "How do you fight someone smarter than yourself?" Rand whispered. "The anser is simple. You make her think that you are sitting down across the table from her, ready to play her game. Then you punch her in the face as hard as you can.”')
-    }
-    if (msg.content.starsWith('>WoTquote')) {
+    if (msg.content.starsWith('>WoTquote2')) {
     msg.channel.send(' "The last days are upon us. If you have grudges, put them behind you. If you have plots, bring them to completion. Make your final plays, for this... this is the end." ')
       }
     if (msg.content.startsWith('>gandalfquote1')) {
-    msg.channel.send(`"YOU...SHALL NOT... PASS!"`);
-      }
-    if (msg.content.startsWith('>gandalfquote2')) {
     msg.channel.send(`"FLY YOU FOOLS!"`);
       }
-    if (msg.content.startsWith('>gandalfquote3')) {
+    if (msg.content.startsWith('>gandalfquote2')) {
     msg.channel.send('"Fool of a Took!"')
     }
 
