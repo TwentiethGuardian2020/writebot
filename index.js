97e6e9d6b5b0b8a330612c9f2072930533b867ae
TwentiethGuardian2020 <66183412+TwentiethGuardian2020@users.noreply.github.com>
2020-06-03 19:41:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ Discord.on('message', async (msg) => {
     if (msg.content.startsWith('>remind')) {
     msg.channel.send(`WRITE, YOU FOOL, WRITE! ${msg.mentions.members.first()}`);
     }
-    if (msg.content.startsWith('>wotquote1')) {
-    msg.channel.send(`"The Wheel weaves as the Wheel wills."`);
-    }
-    if (msg.content.starsWith('>wotquote2')) {
-    msg.channel.send(` "The last days are upon us. If you have grudges, put them behind you. If you have plots, bring them to completion. Make your final plays, for this... this is the end." `);
-      }
-    if (msg.content.startsWith('>gandalfquote1')) {
-    msg.channel.send(`"FLY YOU FOOLS!"`);
+    if (msg.content.startsWith('>wotquote')) {
+    msg.channel.send(` "The Wheel weaves as the Wheel wills." `);
       }
     if (msg.content.startsWith('>gandalfquote2')) {
-    msg.channel.send(`"Fool of a Took!"`);
+    msg.channel.send(` "Fool of a Took!" `);
     }
 
     if (msg.content.startsWith('>createchannel')) {
-      var name = msg.content.replace('>createchannel ', '')
+      var name = msg.content.replace('createchannel ', '')
       msg.guild.channels.create(name)
     }
 });
