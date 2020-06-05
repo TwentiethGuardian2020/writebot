ec0c54a108b4ca2920e806b32ec13f47249bf46f
TwentiethGuardian2020 <66183412+TwentiethGuardian2020@users.noreply.github.com>
2020-06-04 17:12:03 -0700

    added command

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ Discord.on('message', async (msg) => {
     if (msg.content.startsWith('>remind')) {
     msg.channel.send(`WRITE, YOU FOOL, WRITE! ${msg.mentions.members.first()}`);
     }
+  
     if (msg.content.startsWith('>wotquote')) {
     msg.channel.send(` "The Wheel weaves as the Wheel wills." `);
-      }
+    }
     if (msg.content.startsWith('>lotrquote')) {
     msg.channel.send(` "Fool of a Took!" `);
     }
+    if (msg.content.startsWith('>starwarsquote')) {
+      msg.channel.send(` "In a galaxy far far away..." `);
+    }
+   
 
     if (msg.content.startsWith('>createchannel')) {
       var name = msg.content.replace('createchannel ', '')
