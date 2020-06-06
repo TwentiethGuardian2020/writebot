7294c74ae3036de447f0aabbf78d5588740ca47f
TwentiethGuardian2020 <66183412+TwentiethGuardian2020@users.noreply.github.com>
2020-06-05 19:13:08 -0700

    added command

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ Discord.on('message', async (msg) => {
     if (msg.content.startsWith('>remind')) {
     msg.channel.send(`WRITE, YOU FOOL, WRITE! ${msg.mentions.members.first()}`);
     }
-    if (msg.author.bot) return;
     if (msg.content.startsWith('>submit'))
     msg.channel.send(`New submission ${msg.mentions.members.first()}`)
-  
+    if (msg.content.startsWith('>SPOILERZ')) {
+    msg.channel.send(`SPOILER, YOU FOOL! ${msg.mentions.members.first()}`);
+    }
+
     if (msg.content.startsWith('>wotquote')) {
     msg.channel.send(` "The Wheel weaves as the Wheel wills." `);
     }
