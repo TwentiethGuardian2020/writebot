79df9ff5abad64e368b91990691449cdc7cb6223
TwentiethGuardian2020 <66183412+TwentiethGuardian2020@users.noreply.github.com>
2020-06-05 20:37:57 -0700

    fixed command

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ Discord.on('message', async (msg) => {
     }
     if (msg.content.startsWith('>submit'))
     msg.channel.send(`New submission ${msg.mentions.members.first()}`)
-    if (msg.content.startsWith('>SPOILERZ')) {
-    msg.channel.send(`SPOILER, YOU FOOL! ${msg.mentions.members.first()}`);
+    if (msg.content.startsWith('>spoilerz')) {
+    msg.channel.send(`SPOILERS, YOU FOOL! ${msg.mentions.members.first()}`);
     }
     if (msg.content.startsWith('>wotquote')) {
     msg.channel.send(` "The Wheel weaves as the Wheel wills." `);
@@ -28,7 +28,7 @@ Discord.on('message', async (msg) => {
       msg.guild.channels.create(name)
     }
     if (msg.content.startsWith('>help')) {
-      msg.channel.send(` *>remind (@user)*: 'WRITE, YOU FOOL, WRITE'  *>SPOILERZ (@user)*: 'SPOILER, YOU FOOL!'  *>wotquote*: ' "The Wheel weaves as the Wheal wills." '  *>lotrquote*: ' "Fool of a Took!" '  *>starwarsquote*: ' "In a galaxy far far away..." '  *>avatarquote*: ' "My previous girlfriend turned into the moon." "That's rough buddy." ' `)
+      msg.channel.send(`*>remind (@user)*: 'WRITE, YOU FOOL, WRITE' *>spoilerz (@user)*: 'SPOILER, YOU FOOL!' *>wotquote*: ' "The Wheel weaves as the Wheal wills." ' *>lotrquote*: ' "Fool of a Took!" ' *>starwarsquote*: ' "In a galaxy far far away..." '  *>avatarquote*: ' "My previous girlfriend turned into the moon." "That's rough buddy." ' ${msg.mentions.members.first()}`)
     }
   });
 Discord.login(process.env.TOKEN)
